f83b5d3c721c15a50bc977ec430918cea0ba2072
Jonathanleile <164562291+Jonathanleile@users.noreply.github.com>
2024-05-06 12:58:31 -0600

    Add files via upload
    ejercicios 6 de mayo

---
+++ clase fundamentos .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+int n =0;
+int i=0;
+cout << "ingrese numero entero positivo";
+cin>> n;
+while (1<10)
+{  
+    cout << n*i << "\n";
+     i = i+1 ;
+
+
+}
+
+
+}
